99fe269fcd73bf500c1a13dc744d66dbb2244c0a
Alex Currie-Clark <acurrieclark@gmail.com>
2019-11-13 23:43:16 +0000

    Tests updated for PhpUnit v8

--- tests/ExceptionTest.php
+++ tests/ExceptionTest.php
@@ -2,95 +2,78 @@
 
 use acurrieclark\PhpPasswordVerifier\Verifier;
 
-class ExceptionTest extends PHPUnit\Framework\TestCase {
+class ExceptionTest extends PHPUnit\Framework\TestCase
+{
+    public function testNonIntegerMinLength()
+    {
+        $this->expectException(\acurrieclark\PhpPasswordVerifier\Exception\SetConstraintException::class);
+        $this->expectExceptionCode(1);
+        $passwordVerifier = new Verifier();
+        $passwordVerifier->setMinLength('stringNotInteger');
+    }
 
-  /**
-  * @expectedException acurrieclark\PhpPasswordVerifier\Exception\SetConstraintException
-  * @expectedExceptionCode 1
-  */
-  public function testNonIntegerMinLength() {
-    $passwordVerifier = new Verifier();
-    $passwordVerifier->setMinLength('stringNotInteger');
-  }
+    public function testNegativeMinLength()
+    {
+        $this->expectException(\acurrieclark\PhpPasswordVerifier\Exception\SetConstraintException::class);
+        $this->expectExceptionCode(1);
+        $passwordVerifier = new Verifier();
+        $passwordVerifier->setMinLength(-1);
+    }
 
-  /**
-  * @expectedException acurrieclark\PhpPasswordVerifier\Exception\SetConstraintException
-  * @expectedExceptionCode 1
-  */
-  public function testNegativeMinLength() {
-    $passwordVerifier = new Verifier();
-    $passwordVerifier->setMinLength(-1);
-  }
+    public function testNonIntegerMaxLength()
+    {
+        $this->expectException(\acurrieclark\PhpPasswordVerifier\Exception\SetConstraintException::class);
+        $this->expectExceptionCode(2);
+        $passwordVerifier = new Verifier();
+        $passwordVerifier->setMaxLength('stringNotInteger');
+    }
 
-  /**
-  * @expectedException acurrieclark\PhpPasswordVerifier\Exception\SetConstraintException
-  * @expectedExceptionCode 2
-  */
-  public function testNonIntegerMaxLength() {
-    $passwordVerifier = new Verifier();
-    $passwordVerifier->setMaxLength('stringNotInteger');
-  }
+    public function testMaxLengthGreaterThanMinLength()
+    {
+        $this->expectException(\acurrieclark\PhpPasswordVerifier\Exception\SetConstraintException::class);
+        $this->expectExceptionCode(3);
+        $passwordVerifier = new Verifier();
+        $passwordVerifier->setMinLength(8);
+        $passwordVerifier->setMaxLength(7);
+    }
 
-  /**
-  * @expectedException acurrieclark\PhpPasswordVerifier\Exception\SetConstraintException
-  * @expectedExceptionCode 3
-  */
-  public function testMaxLengthGreaterThanMinLength() {
-    $passwordVerifier = new Verifier();
-    $passwordVerifier->setMinLength(8);
-    $passwordVerifier->setMaxLength(7);
-  }
+    public function testCheckContainsLettersInputIsBoolean()
+    {
+        $this->expectException(\acurrieclark\PhpPasswordVerifier\Exception\SetConstraintException::class);
+        $this->expectExceptionCode(4);
+        $passwordVerifier = new Verifier();
+        $passwordVerifier->setCheckContainsLetters('hello');
+    }
 
-  /**
-  * @expectedException acurrieclark\PhpPasswordVerifier\Exception\SetConstraintException
-  * @expectedExceptionCode 4
-  */
+    public function testCheckContainsNumbersInputIsBoolean()
+    {
+        $this->expectException(\acurrieclark\PhpPasswordVerifier\Exception\SetConstraintException::class);
+        $this->expectExceptionCode(5);
+        $passwordVerifier = new Verifier();
+        $passwordVerifier->setCheckContainsNumbers('hello');
+    }
 
-  public function testCheckContainsLettersInputIsBoolean() {
-    $passwordVerifier = new Verifier();
-    $passwordVerifier->setCheckContainsLetters('hello');
-  }
+    public function testCheckContainsSpecialChrsInputIsBoolean()
+    {
+        $this->expectException(\acurrieclark\PhpPasswordVerifier\Exception\SetConstraintException::class);
+        $this->expectExceptionCode(6);
+        $passwordVerifier = new Verifier();
+        $passwordVerifier->setCheckContainsSpecialChrs('hello');
+    }
 
-  /**
-  * @expectedException acurrieclark\PhpPasswordVerifier\Exception\SetConstraintException
-  * @expectedExceptionCode 8
-  */
+    public function testCheckBlacklistInputIsBoolean()
+    {
+        $this->expectException(\acurrieclark\PhpPasswordVerifier\Exception\SetConstraintException::class);
+        $this->expectExceptionCode(7);
+        $passwordVerifier = new Verifier();
+        $passwordVerifier->setCheckBlacklist('hello');
+    }
 
-  public function testCheckContainsCapitalsInputIsBoolean() {
-    $passwordVerifier = new Verifier();
-    $passwordVerifier->setCheckContainsCapitals('hello');
-  }
-
-  /**
-  * @expectedException acurrieclark\PhpPasswordVerifier\Exception\SetConstraintException
-  * @expectedExceptionCode 5
-  */
-
-  public function testCheckContainsNumbersInputIsBoolean() {
-    $passwordVerifier = new Verifier();
-    $passwordVerifier->setCheckContainsNumbers('hello');
-  }
-
-
-  /**
-  * @expectedException acurrieclark\PhpPasswordVerifier\Exception\SetConstraintException
-  * @expectedExceptionCode 6
-  */
-
-  public function testCheckContainsSpecialChrsInputIsBoolean() {
-    $passwordVerifier = new Verifier();
-    $passwordVerifier->setCheckContainsSpecialChrs('hello');
-  }
-
-  /**
-  * @expectedException acurrieclark\PhpPasswordVerifier\Exception\SetConstraintException
-  * @expectedExceptionCode 7
-  */
-
-  public function testCheckBlacklistInputIsBoolean() {
-    $passwordVerifier = new Verifier();
-    $passwordVerifier->setCheckBlacklist('hello');
-  }
+    public function testCheckContainsCapitalsInputIsBoolean()
+    {
+        $this->expectException(\acurrieclark\PhpPasswordVerifier\Exception\SetConstraintException::class);
+        $this->expectExceptionCode(8);
+        $passwordVerifier = new Verifier();
+        $passwordVerifier->setCheckContainsCapitals('hello');
+    }
 }
-
-?>
--- tests/VerifyTest.php
+++ tests/VerifyTest.php
@@ -2,163 +2,178 @@
 
 use acurrieclark\PhpPasswordVerifier\Verifier;
 
-class VerifyTest extends PHPUnit\Framework\TestCase {
+class VerifyTest extends PHPUnit\Framework\TestCase
+{
+    public function testStandardPass()
+    {
+        $passwordVerifier = new Verifier();
+        $this->assertTrue($passwordVerifier->checkPassword('1245'));
+        $this->assertEmpty($passwordVerifier->getErrors());
+    }
 
-  public function testStandardPass() {
-    $passwordVerifier = new Verifier();
-    $this->assertTrue($passwordVerifier->checkPassword('1245'));
-    $this->assertEmpty($passwordVerifier->getErrors());
-  }
+    public function testMinLengthFail()
+    {
+        $passwordVerifier = new Verifier();
+        $passwordVerifier->setMinLength(6);
+        $this->assertFalse($passwordVerifier->checkPassword('12345'));
+        $this->assertNotEmpty($passwordVerifier->getErrors()['minLength']);
+    }
 
-  public function testMinLengthFail() {
-    $passwordVerifier = new Verifier();
-    $passwordVerifier->setMinLength(6);
-    $this->assertFalse($passwordVerifier->checkPassword('12345'));
-    $this->assertNotEmpty($passwordVerifier->getErrors()['minLength']);
-  }
+    public function testMinLengthPass()
+    {
+        $passwordVerifier = new Verifier();
+        $passwordVerifier->setMinLength(6);
+        $this->assertTrue($passwordVerifier->checkPassword('1234567'));
+        $this->assertEmpty($passwordVerifier->getErrors());
+    }
 
-  public function testMinLengthPass() {
-    $passwordVerifier = new Verifier();
-    $passwordVerifier->setMinLength(6);
-    $this->assertTrue($passwordVerifier->checkPassword('1234567'));
-    $this->assertEmpty($passwordVerifier->getErrors());
-  }
+    public function testChangeMinLength()
+    {
+        $passwordVerifier = new Verifier();
+        $passwordVerifier->setMinLength(8);
+        $this->assertFalse($passwordVerifier->checkPassword('1234567'));
+        $this->assertNotEmpty($passwordVerifier->getErrors()['minLength']);
+    }
 
-  public function testChangeMinLength() {
-    $passwordVerifier = new Verifier();
-    $passwordVerifier->setMinLength(8);
-    $this->assertFalse($passwordVerifier->checkPassword('1234567'));
-    $this->assertNotEmpty($passwordVerifier->getErrors()['minLength']);
-  }
+    public function testCancelMinLength()
+    {
+        $passwordVerifier = new Verifier();
+        $passwordVerifier->setMinLength(false);
+        $this->assertTrue($passwordVerifier->checkPassword('1'));
+        $this->assertEmpty($passwordVerifier->getErrors());
+    }
 
-  public function testCancelMinLength() {
-    $passwordVerifier = new Verifier();
-    $passwordVerifier->setMinLength(false);
-    $this->assertTrue($passwordVerifier->checkPassword('1'));
-    $this->assertEmpty($passwordVerifier->getErrors());
-  }
+    public function testMaxLengthFail()
+    {
+        $passwordVerifier = new Verifier();
+        $passwordVerifier->setMaxLength(8);
+        $this->assertFalse($passwordVerifier->checkPassword('123456789'));
+        $this->assertNotEmpty($passwordVerifier->getErrors()['maxLength']);
+    }
 
-  public function testMaxLengthFail() {
-    $passwordVerifier = new Verifier();
-    $passwordVerifier->setMaxLength(8);
-    $this->assertFalse($passwordVerifier->checkPassword('123456789'));
-    $this->assertNotEmpty($passwordVerifier->getErrors()['maxLength']);
-  }
+    public function testMaxLengthPass()
+    {
+        $passwordVerifier = new Verifier();
+        $passwordVerifier->setMaxLength(8);
+        $this->assertTrue($passwordVerifier->checkPassword('12345678'));
+        $this->assertEmpty($passwordVerifier->getErrors());
+    }
 
-  public function testMaxLengthPass() {
-    $passwordVerifier = new Verifier();
-    $passwordVerifier->setMaxLength(8);
-    $this->assertTrue($passwordVerifier->checkPassword('12345678'));
-    $this->assertEmpty($passwordVerifier->getErrors());
-  }
+    public function testContainsLetterFail()
+    {
+        $passwordVerifier = new Verifier();
+        $passwordVerifier->setCheckContainsLetters(true);
+        $this->assertFalse($passwordVerifier->checkPassword('1234'));
+        $this->assertNotEmpty($passwordVerifier->getErrors()['letters']);
+    }
 
-  public function testContainsLetterFail() {
-    $passwordVerifier = new Verifier();
-    $passwordVerifier->setCheckContainsLetters(true);
-    $this->assertFalse($passwordVerifier->checkPassword('1234'));
-    $this->assertNotEmpty($passwordVerifier->getErrors()['letters']);
-  }
+    public function testContainsLetterPass()
+    {
+        $passwordVerifier = new Verifier();
+        $passwordVerifier->setCheckContainsLetters(true);
+        $this->assertTrue($passwordVerifier->checkPassword('1234asdf'));
+        $this->assertEmpty($passwordVerifier->getErrors());
+    }
 
-  public function testContainsLetterPass() {
-    $passwordVerifier = new Verifier();
-    $passwordVerifier->setCheckContainsLetters(true);
-    $this->assertTrue($passwordVerifier->checkPassword('1234asdf'));
-    $this->assertEmpty($passwordVerifier->getErrors());
-  }
+    public function testContainsCapitalsFail()
+    {
+        $passwordVerifier = new Verifier();
+        $passwordVerifier->setCheckContainsCapitals(true);
+        $this->assertFalse($passwordVerifier->checkPassword('asdf'));
+        $this->assertNotEmpty($passwordVerifier->getErrors()['capitals']);
+    }
 
-  public function testContainsCapitalsFail() {
-    $passwordVerifier = new Verifier();
-    $passwordVerifier->setCheckContainsCapitals(true);
-    $this->assertFalse($passwordVerifier->checkPassword('asdf'));
-    $this->assertNotEmpty($passwordVerifier->getErrors()['capitals']);
-  }
+    public function testContainsCapitalsPass()
+    {
+        $passwordVerifier = new Verifier();
+        $passwordVerifier->setCheckContainsCapitals(true);
+        $this->assertTrue($passwordVerifier->checkPassword('Asdf'));
+        $this->assertEmpty($passwordVerifier->getErrors());
+    }
 
-  public function testContainsCapitalsPass() {
-    $passwordVerifier = new Verifier();
-    $passwordVerifier->setCheckContainsCapitals(true);
-    $this->assertTrue($passwordVerifier->checkPassword('Asdf'));
-    $this->assertEmpty($passwordVerifier->getErrors());
-  }
+    public function testContainsNumberFail()
+    {
+        $passwordVerifier = new Verifier();
+        $passwordVerifier->setCheckContainsNumbers(true);
+        $this->assertFalse($passwordVerifier->checkPassword('asdf'));
+        $this->assertNotEmpty($passwordVerifier->getErrors()['numbers']);
+    }
 
-  public function testContainsNumberFail() {
-    $passwordVerifier = new Verifier();
-    $passwordVerifier->setCheckContainsNumbers(true);
-    $this->assertFalse($passwordVerifier->checkPassword('asdf'));
-    $this->assertNotEmpty($passwordVerifier->getErrors()['numbers']);
-  }
+    public function testContainsNumberPass()
+    {
+        $passwordVerifier = new Verifier();
+        $passwordVerifier->setCheckContainsNumbers(true);
+        $this->assertTrue($passwordVerifier->checkPassword('1234asdf'));
+        $this->assertEmpty($passwordVerifier->getErrors());
+    }
 
-  public function testContainsNumberPass() {
-    $passwordVerifier = new Verifier();
-    $passwordVerifier->setCheckContainsNumbers(true);
-    $this->assertTrue($passwordVerifier->checkPassword('1234asdf'));
-    $this->assertEmpty($passwordVerifier->getErrors());
-  }
+    public function testContainsSpecialChrsFail()
+    {
+        $passwordVerifier = new Verifier();
+        $passwordVerifier->setCheckContainsSpecialChrs(true);
+        $this->assertFalse($passwordVerifier->checkPassword('asdf'));
+        $this->assertNotEmpty($passwordVerifier->getErrors()['specialChrs']);
+    }
 
-  public function testContainsSpecialChrsFail() {
-    $passwordVerifier = new Verifier();
-    $passwordVerifier->setCheckContainsSpecialChrs(true);
-    $this->assertFalse($passwordVerifier->checkPassword('asdf'));
-    $this->assertNotEmpty($passwordVerifier->getErrors()['specialChrs']);
-  }
+    public function testContainsSpecialChrsPass()
+    {
+        $passwordVerifier = new Verifier();
+        $passwordVerifier->setCheckContainsSpecialChrs(true);
+        $this->assertTrue($passwordVerifier->checkPassword('{'));
+        $this->assertTrue($passwordVerifier->checkPassword('}'));
+        $this->assertTrue($passwordVerifier->checkPassword('('));
+        $this->assertTrue($passwordVerifier->checkPassword(')'));
+        $this->assertTrue($passwordVerifier->checkPassword('['));
+        $this->assertTrue($passwordVerifier->checkPassword(']'));
+        $this->assertTrue($passwordVerifier->checkPassword('#'));
+        $this->assertTrue($passwordVerifier->checkPassword(','));
+        $this->assertTrue($passwordVerifier->checkPassword(':'));
+        $this->assertTrue($passwordVerifier->checkPassword(';'));
+        $this->assertTrue($passwordVerifier->checkPassword('^'));
+        $this->assertTrue($passwordVerifier->checkPassword('.'));
+        $this->assertTrue($passwordVerifier->checkPassword('?'));
+        $this->assertTrue($passwordVerifier->checkPassword('!'));
+        $this->assertTrue($passwordVerifier->checkPassword('|'));
+        $this->assertTrue($passwordVerifier->checkPassword('&'));
+        $this->assertTrue($passwordVerifier->checkPassword('_'));
+        $this->assertTrue($passwordVerifier->checkPassword('`'));
+        $this->assertTrue($passwordVerifier->checkPassword('~'));
+        $this->assertTrue($passwordVerifier->checkPassword('@'));
+        $this->assertTrue($passwordVerifier->checkPassword('$'));
+        $this->assertTrue($passwordVerifier->checkPassword('%'));
+        $this->assertTrue($passwordVerifier->checkPassword('/'));
+        $this->assertTrue($passwordVerifier->checkPassword('\\'));
+        $this->assertTrue($passwordVerifier->checkPassword('='));
+        $this->assertTrue($passwordVerifier->checkPassword('+'));
+        $this->assertTrue($passwordVerifier->checkPassword('-'));
+        $this->assertTrue($passwordVerifier->checkPassword('*'));
+        $this->assertTrue($passwordVerifier->checkPassword('"'));
+        $this->assertTrue($passwordVerifier->checkPassword("'"));
+        $this->assertEmpty($passwordVerifier->getErrors());
+    }
 
-  public function testContainsSpecialChrsPass() {
-    $passwordVerifier = new Verifier();
-    $passwordVerifier->setCheckContainsSpecialChrs(true);
-    $this->assertTrue($passwordVerifier->checkPassword("{"));
-    $this->assertTrue($passwordVerifier->checkPassword("}"));
-    $this->assertTrue($passwordVerifier->checkPassword("("));
-    $this->assertTrue($passwordVerifier->checkPassword(")"));
-    $this->assertTrue($passwordVerifier->checkPassword("["));
-    $this->assertTrue($passwordVerifier->checkPassword("]"));
-    $this->assertTrue($passwordVerifier->checkPassword("#"));
-    $this->assertTrue($passwordVerifier->checkPassword(","));
-    $this->assertTrue($passwordVerifier->checkPassword(":"));
-    $this->assertTrue($passwordVerifier->checkPassword(";"));
-    $this->assertTrue($passwordVerifier->checkPassword("^"));
-    $this->assertTrue($passwordVerifier->checkPassword("."));
-    $this->assertTrue($passwordVerifier->checkPassword("?"));
-    $this->assertTrue($passwordVerifier->checkPassword("!"));
-    $this->assertTrue($passwordVerifier->checkPassword("|"));
-    $this->assertTrue($passwordVerifier->checkPassword("&"));
-    $this->assertTrue($passwordVerifier->checkPassword("_"));
-    $this->assertTrue($passwordVerifier->checkPassword("`"));
-    $this->assertTrue($passwordVerifier->checkPassword("~"));
-    $this->assertTrue($passwordVerifier->checkPassword("@"));
-    $this->assertTrue($passwordVerifier->checkPassword("$"));
-    $this->assertTrue($passwordVerifier->checkPassword("%"));
-    $this->assertTrue($passwordVerifier->checkPassword("/"));
-    $this->assertTrue($passwordVerifier->checkPassword("\\"));
-    $this->assertTrue($passwordVerifier->checkPassword("="));
-    $this->assertTrue($passwordVerifier->checkPassword("+"));
-    $this->assertTrue($passwordVerifier->checkPassword("-"));
-    $this->assertTrue($passwordVerifier->checkPassword("*"));
-    $this->assertTrue($passwordVerifier->checkPassword('"'));
-    $this->assertTrue($passwordVerifier->checkPassword("'"));
-    $this->assertEmpty($passwordVerifier->getErrors());
-  }
+    public function testBlacklistFail()
+    {
+        $passwordVerifier = new Verifier();
+        $passwordVerifier->setCheckBlacklist(true);
+        $this->assertFalse($passwordVerifier->checkPassword('asdf'));
+        $this->assertNotEmpty($passwordVerifier->getErrors()['blacklist']);
+    }
 
-  public function testBlacklistFail() {
-    $passwordVerifier = new Verifier();
-    $passwordVerifier->setCheckBlacklist(true);
-    $this->assertFalse($passwordVerifier->checkPassword('asdf'));
-    $this->assertNotEmpty($passwordVerifier->getErrors()['blacklist']);
-  }
+    public function testBlacklistPass()
+    {
+        $passwordVerifier = new Verifier();
+        $passwordVerifier->setCheckBlacklist(true);
+        $this->assertTrue($passwordVerifier->checkPassword('heebegeebeesyoulittlebeauty'));
+        $this->assertEmpty($passwordVerifier->getErrors());
+    }
 
-  public function testBlacklistPass() {
-    $passwordVerifier = new Verifier();
-    $passwordVerifier->setCheckBlacklist(true);
-    $this->assertTrue($passwordVerifier->checkPassword('heebegeebeesyoulittlebeauty'));
-    $this->assertEmpty($passwordVerifier->getErrors());
-  }
-
-  public function testChainedConstraints() {
-    $passwordVerifier = new Verifier();
-    $passwordVerifier->setCheckBlacklist(true)
+    public function testChainedConstraints()
+    {
+        $passwordVerifier = new Verifier();
+        $passwordVerifier->setCheckBlacklist(true)
                      ->setMinLength(6)
                      ->setMaxLength(12);
-    $this->assertFalse($passwordVerifier->checkPassword('1234567890asdfg'));
-  }
-
+        $this->assertFalse($passwordVerifier->checkPassword('1234567890asdfg'));
+    }
 }
-
-?>
